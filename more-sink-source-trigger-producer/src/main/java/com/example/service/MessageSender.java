3cb89d5a62785ed62be32c072f2030a6a54c2e3b
xiaohui.c.liu <xiaohui.c.liu@CDC112093.dir.svc.accenture.com>
2017-06-15 17:24:16 +0800

    提交messageSender

--- more-sink-source-trigger-producer/src/main/java/com/example/service/MessageSender.java
+++ more-sink-source-trigger-producer/src/main/java/com/example/service/MessageSender.java
@@ -7,12 +7,7 @@ import com.example.data.MultiSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.messaging.support.MessageBuilder;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-
-/**
- * Created by pkpk1234 on 2017/5/25.
- */
 
 @Service
 @EnableBinding(MultiSource.class)
